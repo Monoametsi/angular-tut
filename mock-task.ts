d017df1af81a602fafd78860dce6f4911302ee2c
Junior <216026633@student.uj.ac.za>
2021-12-19 19:26:45 +0200

    Add task ts files

---
+++ mock-task.ts
@@ -0,0 +1,25 @@
+export const tasks = [
+  {
+	"id": 1,
+	"text": " Shopping"
+  },
+  {
+	"id": 2,
+	"text": " lunch"
+  }
+  ,
+  {
+	"id": 3,
+	"text": " work"
+  }
+  ,
+  {
+	"id": 4,
+	"text": " travel"
+  }
+  ,
+  {
+	"id": 5,
+	"text": " eat"
+  }
+]
