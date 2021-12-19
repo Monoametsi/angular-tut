966dd37308376e55eb06493da08195d045f2c4bf
Junior <216026633@student.uj.ac.za>
2021-12-19 21:52:31 +0200

    Add tasks objects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,6 @@ export class AppComponent {
   title:string = 'TODOLIST';
   
   AddTask(){
-	console.log('Add Task');
+	alert('Add Task');
   }
 }
---
+++ src/app/mock-task.ts
@@ -0,0 +1,27 @@
+import {Task} from './task'
+
+export const TASKS: Task[] = [
+  {
+	id: 1,
+	task: " Shopping"
+  },
+  {
+	id: 2,
+	task: " lunch"
+  }
+  ,
+  {
+	id: 3,
+	task: " work"
+  }
+  ,
+  {
+	id: 4,
+	task: " travel"
+  }
+  ,
+  {
+	id: 55,
+	task: " eat"
+  }
+]
